9e2e0d3fa86adf52f4d69aefe94045e819affbbf
trgr <trygve@grimnes.org>
2016-06-21 13:47:07 +0200

    Jquerify

--- test.js
+++ test.js
@@ -2,18 +2,13 @@
 $( window ).load(function()		{
 	var f = document.getElementById("new-listing-link");
 	f.innerHTML = "Selg gavekort";
-                                        
-//	var title = document.getElementById( "listing_title" )
-//	title.style.display = "none";
-
+	
 	$('label[for="listing_title"]').hide ();
-	
-	var info = document.getElementById( "info-text-container" );
-	$('label[for="listing_description"]').hide ();
-	info.style.display = "none";
-	
-	var desc = document.getElementById( "listing_description" );
 
-	desc.style.display = "none";
+	$("#info-text-container").hide();
+	
+	$('label[for="listing_description"]').hide ();
+
+	$("#listing_description").hide();	
 	
 });
