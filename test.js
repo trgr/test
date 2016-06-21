8f3c6f021ff32a1ae01dfab16677d1d8a6bf0b88
trgr <trygve@grimnes.org>
2016-06-21 11:06:14 +0200

    hide

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
 window.onload = function(){
 	var f = document.getElementById("new-listing-link");
 	f.innerHTML = "Selg gavekort";
+
+	var title = document.getElementById( "listing-title" );
+
+	title.style.display = "none";
+	
 }
