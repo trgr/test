822154bc93a912189c49f55619031de2dce25360
trgr <trygve@grimnes.org>
2016-06-21 10:49:19 +0200

    Foo

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 window.onload = function(){
 	var f = document.getElementById("new-listing-link");
 	f.innerHTML = "FoO";
-	console.log("Hello");
+	console.log("Hello..");
 	console.log( f );
+	console.log( "foo ");
 }
