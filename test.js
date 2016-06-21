7f07036e62af24c51d38dddbf2060045e1229b99
trgr <trygve@grimnes.org>
2016-06-21 11:09:48 +0200

    Foo

--- test.js
+++ test.js
@@ -5,5 +5,13 @@ window.onload = function(){
 	var title = document.getElementById( "listing_title" );
 
 	title.style.display = "none";
+
+	var info = document.getElementById( "info-text-container" );
+
+	info.style.display = "none";
+	
+	var desc = document.getElementById( "listing_description" );
+
+	desc.style.display = "none";
 	
 }
