cf1fc1508c3400c1eddc6b3eaec56261215d072e
trgr <trygve@grimnes.org>
2016-06-21 11:25:25 +0200

    Foo

--- test.js
+++ test.js
@@ -1,13 +1,14 @@
-window.onload = function(){
+document.addEventListener("DOMContentLoaded", function(event) { 
 	var f = document.getElementById("new-listing-link");
 	f.innerHTML = "Selg gavekort";
-
-	var title = document.getElementById( "listing_title" );
-
+                                        
+	var title = document.getElementById( "listing_title" )
 	title.style.display = "none";
 
+	$('label[for="listing_title"]').hide ();
+	
 	var info = document.getElementById( "info-text-container" );
-
+		$('label[for="listing_description"]').hide ();
 	info.style.display = "none";
 	
 	var desc = document.getElementById( "listing_description" );
